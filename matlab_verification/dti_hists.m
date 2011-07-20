6a4b12a43601bb0ed405c7a2c2a8f39b9281beb2
Ben_Kandel <bkandelemail-github@yahoo.com>
2011-07-20 15:58:50 -0400

    Modified "dti_distance_*" to only calculate histograms, not distance statistics.

--- matlab_verification/dti_hists.m
+++ matlab_verification/dti_hists.m
@@ -1,8 +1,8 @@
-% This mfile is intended to load the images given to us to test the various
-% distances, calculate the distance between test voxels and the test
-% tracts, and see how the various distances compare. 
+% This mfile will generate the 2-D histograms of
+% the angles and principal eigenvectors of DTI 
+% data and save them. 
 
-function dti_distance_presetSigma(folder, fileName, sigma)
+function dti_hists(folder, fileName, sigma, numBins)
  
 sigma = sigma; 
 subj = fileName; 
@@ -24,12 +24,15 @@ prinEigVector = getPrinEigVec(eigVectors, eigValues);
 
 
 
+% Define minima and maxima for phi and theta: 
+% Phi defined as angle from azimuth, theta 
+% defined as angle from horizontal (x-axis). 
+% Phi (vertical angle) ranges from 0 to pi, theta
+% from 0 to 2 * pi. 
+minXY = [0 0]; 
+maxXY = [pi 2*pi];
 
-% Compute nonparametric population density function using kde2d. 
-minXY = [-90 -90]; 
-maxXY = [90 90];
-
-numBins = 2^7; 
+numBins = numBins; 
 
 % Define 2-D Gaussian filter for convolution
 gaussFilter = fspecial('gaussian', (sigma * 6 + 1), sigma); 
@@ -50,6 +53,7 @@ for i = 1:length(angles)
     pdfmat(:, :, i) = conv2(pdfmat(:, :, i), gaussFilter, 'same'); 
     anglePdf{i} = pdfmat(:, :, i);
     anglePdfLog{i} = log(pdfmat(:, :, i)); 
+    csvwrite(strcat('hist_', num2str(i), '.csv'), histmat(:, :, i)); 
 end
 
 
@@ -61,7 +65,7 @@ end
 %     ylabel('\theta');
 % end
 
-
+%{
 for i = 1:20
     angle1bin{i} = angles{i}(:,1); 
     angle1bin{i} = angle1bin{i} * numBins / 180; 
@@ -73,7 +77,7 @@ for i = 1:20
     angle2bin{i} = floor(angle2bin{i}); 
     angle2bin{i} = angle2bin{i} + numBins/2+1; 
 end
-
+%}
 
 %% Do same for lambda
 %{
@@ -127,7 +131,7 @@ end
 
 %%
 
-
+%{
 for i = 1:10
     for j = 1:10
         for k = 1:length(angle1bin{i})
@@ -197,6 +201,8 @@ meanDiag = diag(meanProbAngleLog{2});
 maxMean = min(meanProbAngleLog{2}'); 
 missesMean = meanDiag ~= maxMean'; 
 sum(missesMean)
+%}
+
 %{
 % medProb
 % medDiag = diag(medProb); 
@@ -244,153 +250,5 @@ sum(missesMean)
 
 %% Calculate Distances
 %Mahalanobis
-
-for i = 1:10
-    for j = 1:10
-        distMahal{1}(i,j) = mean(mahal(Tensor{i},Tensor{j})); 
-    end
-end
-
-
-
-mahalDiag = diag(distMahal{1}); 
-minMahal = min(distMahal{1}'); 
-missesMahal = mahalDiag ~= minMahal'; 
-sum(missesMahal)
-
-%{
- progressbar('Computing MCD Norm, Subject 1'); 
-for i = 1:10
-    for j = 1:10 
-        distMCD{1}(i,j) = mean(mcdmahalNoPlot(Tensor{i},Tensor{j})); 
-        progressbar(((i-1)*10+j) / 100);
-    end
-end
-mcdDiag = diag(distMCD{1}); 
-minMCD = min(distMCD{1}'); 
-missesMCD = mcdDiag ~= minMCD'; 
-sum(missesMCD)
-%}
-for i = 11:20
-    for j = 11:20
-        distMahal{2}(i-10,j-10) = mean(mahal(Tensor{i},Tensor{j})); 
-    end
-end
-
-
-
-mahalDiag = diag(distMahal{2}); 
-minMahal = min(distMahal{2}'); 
-missesMahal = mahalDiag ~= minMahal'; 
-sum(missesMahal)
-%{
-progressbar('Computing MCD Norm, Subject 2'); 
-
-for i = 11:20
-    for j = 11:20 
-        distMCD{2}(i-10,j-10) = mean(mcdmahalNoPlot(Tensor{i},Tensor{j})); 
-        progressbar( ((i-11) * 10 + j-10) / 100)
-    end
-end
-mcdDiag = diag(distMCD{2}); 
-minMCD = min(distMCD{2}'); 
-missesMCD = mcdDiag ~= minMCD'; 
-sum(missesMCD)
-%}
-
-%{
-xlswrite('distanceCompare.xls',distMahal,'totalMahal'); 
-xlswrite('distanceCompare.xls', distMCD, 'totalMCD'); 
-%}
-
-
-xlswrite(subj, meanProbAngleLog{1},'meanProbAngleLog1'); 
-xlswrite(subj, meanProbAngleLog{2}, 'meanProbAngleLog2'); 
-xlswrite(subj, distMahal{1}, 'distMahal1'); 
-xlswrite(subj, distMahal{2}, 'distMahal2'); 
-%xlswrite(subj, distMCD{1}, 'distMCD1'); 
-%xlswrite(subj, distMCD{2}, 'distMCD2'); 
-
-%{
-lCst2Ilf = mahal(left_cst_TensorMat, left_ilf_TensorMat);
-lCst2rCst = mahal(left_cst_TensorMat, right_cst_TensorMat);
-lCst2lCst = mahal(left_cst_TensorMat, left_cst_TensorMat);
-lCst2Slf = mahal(left_cst_TensorMat, left_slf_TensorMat); 
-lCst2lUnc = mahal(left_cst_TensorMat, left_unc_TensorMat); 
-
-% Mahalanobis with MCD covariance matrix
-lCst2lCstMCD = mcdmahal(double(left_cst_TensorMat), double(left_cst_TensorMat));
-lCst2IlfMCD = mcdmahal(double(left_cst_TensorMat), double(left_ilf_TensorMat));
-lCst2rCstMCD = mcdmahal(double(left_cst_TensorMat), double(right_cst_TensorMat));
-lCst2SlfMCD = mcdmahal(double(left_cst_TensorMat), double(left_slf_TensorMat)); 
-lCst2lUncMCD = mcdmahal(double(left_cst_TensorMat), double(left_unc_TensorMat)); 
-
-%% Plot
-[lCst2lCstHist, lCst2lCstBin] = hist(lCst2lCst, 1000); 
-[lCst2IlfHist, lCst2IlfBin] = hist(lCst2Ilf, 1000); 
-[lCst2rCstHist, lCst2rCstBin] = hist(lCst2rCst, 1000); 
-[lCst2SlfHist, lCst2SlfBin] = hist(lCst2Slf, 1000); 
-[lCst2lUncHist, lCst2lUncBin]  = hist(lCst2lUnc, 1000); 
-
-[lCst2lCstHistMCD, lCst2lCstBinMCD] = hist(lCst2lCstMCD, 1000); 
-[lCst2IlfHistMCD, lCst2IlfBinMCD] = hist(lCst2IlfMCD, 1000); 
-[lCst2rCstHistMCD, lCst2rCstBinMCD] = hist(lCst2rCstMCD, 1000); 
-[lCst2SlfHistMCD, lCst2SlfBinMCD] = hist(lCst2SlfMCD, 1000); 
-[lCst2lUncHistMCD, lCst2lUncBinMCD]  = hist(lCst2lUncMCD, 1000); 
-
-figure; 
-plot(lCst2IlfBin, lCst2IlfHist, lCst2rCstBin, lCst2rCstHist, ...
-    lCst2SlfBin, lCst2SlfHist, lCst2lUncBin, lCst2lUncHist,lCst2lCstBin, lCst2lCstHist); 
-legend('Left CST to Left ILF', 'Left CST to Right CST', 'Left CST to Left SLF', ...
-    'Left CST to Left Uncinate', 'Left CST to Left CST')
-
-
-axis([0 50 0 1000])
-xlabel('Distance'); ylabel('Counts'); 
-title('Histogram of Mahalanobis Distance Between Various Tracts')
-saveas(gcf, 'mahalDistance.png')
-
-figure; 
-
-plot(lCst2IlfBinMCD, lCst2IlfHistMCD, lCst2rCstBinMCD, lCst2rCstHistMCD, ...
-    lCst2SlfBinMCD, lCst2SlfHistMCD, lCst2lUncBinMCD, lCst2lUncHistMCD, lCst2lCstBinMCD, lCst2lCstHistMCD); 
-legend('Left CST to Left ILF', 'Left CST to Right CST', 'Left CST to Left SLF', ...
-    'Left CST to Left Uncinate', 'Left CST to Left CST')
-
-axis([0 50 0 1000])
-xlabel('Distance'); ylabel('Counts'); 
-title('Histogram of MCD Mahalanobis Distance Between Various Tracts')
-saveas(gcf, 'mcdMahalDistance.png')
-
-% Create bar graph of average distances
-avgDist(1, 1) = mean(lCst2rCst); 
-avgDist(1,2) = mean(lCst2rCstMCD); 
-avgDist(2,1) = mean(lCst2Ilf); 
-avgDist(2,2) = mean(lCst2IlfMCD); 
-avgDist(3,1) = mean(lCst2Slf); 
-avgDist(3,2) = mean(lCst2SlfMCD); 
-avgDist(4,1) = mean(lCst2lUnc); 
-avgDist(4,2) = mean(lCst2lUncMCD);
-
-for i = 1:4
-distDif(i, :) = avgDist(i,:) - avgDist(1,:);
-end
-
-medDist(1, 1) = median(lCst2rCst); 
-medDist(1,2) = median(lCst2rCstMCD); 
-medDist(2,1) = median(lCst2Ilf); 
-medDist(2,2) = median(lCst2IlfMCD); 
-medDist(3,1) = median(lCst2Slf); 
-medDist(3,2) = median(lCst2SlfMCD); 
-medDist(4,1) = median(lCst2lUnc); 
-medDist(4,2) = median(lCst2lUncMCD);
-
-for i = 1:4
-distDifMed(i, :) = medDist(i,:) - medDist(1,:);
-end
-
-figure; bar(distDif(2:4,:)); 
-
-%}
 
     
