dcbfe2cac399c7774817dd980198cd9288f2d455
Ben_Kandel <bkandelemail-github@yahoo.com>
2011-08-01 13:49:56 -0400

    Initial commit of DTI MVSEG paper.

--- AtroposMin_subj113.sh
+++ AtroposMin_subj113.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-for file in ~/Dropbox/MVSEG/dti_distance_project/kirby/subj_113/KKI2009-30-DWI_left*
+for file in KKI2009-30-DWI_left*
 do
 echo "Processing file $file" 
-./seg_dti_manual_init_atroposMin.sh ~/Dropbox/MVSEG/dti_distance_project/kirby/subj_113/KKI2009-30-DWI_dt.nii Tensor out $file 2
+./seg_dti_manual_init_atroposMin.sh KKI2009-30-DWI_dt.nii Tensor out $file 2
 FILE_NAME=$(basename $file .nii)
 mkdir $FILE_NAME
-mv out* $FILE_NAME
+mv hist* $FILE_NAME
 done
